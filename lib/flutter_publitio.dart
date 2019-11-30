3b789c9b8cf743749bcffdc60728feb878127d1a
Johnny <syonip@gmail.com>
2019-11-30 22:34:18 +0200

    added dartdoc

--- lib/flutter_publitio.dart
+++ lib/flutter_publitio.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 class FlutterPublitio {
   static const MethodChannel _channel = const MethodChannel('flutter_publitio');
 
+  /// Call [configure] with your publitio key and secret before using the APIs
   static Future<void> configure(String apiKey, String apiSecret) async {
     await _channel.invokeMethod('configure', {
       "apiKey": apiKey,
@@ -11,6 +12,8 @@ class FlutterPublitio {
     });
   }
 
+  /// Uploads a file to publitio. 
+  /// Returns a [map] with information about the uploaded file.
   static Future<dynamic> uploadFile(String path, options) async {
     final result = await _channel
         .invokeMethod('uploadFile', {"path": path, "options": options});
